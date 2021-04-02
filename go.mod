fc912c8cf06ff73ebc42ab7be780d6eaaf6767a3
zzy <1359976577@qq.com>
2021-04-02 19:03:29 +0800

    version1.0,Added SignedCertificateTimestampLog struct

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/zmap/zcrypto
+
+require (
+	github.com/google/certificate-transparency-go v1.1.1
+	github.com/mreiferson/go-httpclient v0.0.0-20160630210159-31f0106b4474
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.6.1
+	github.com/weppos/publicsuffix-go v0.4.0
+	github.com/zmap/rc2 v0.0.0-20131011165748-24b9757f5521
+	github.com/zmap/zcertificate v0.0.0-20180516150559-0e3d58b1bac4
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15
+)
+
+go 1.13
---
+++ x509/revocation/ocsp/ocspstapling.go
@@ -0,0 +1,120 @@
+package ocsp
+
+import(
+	"encoding/asn1"
+	"encoding/base64"
+	"errors"
+	"github.com/zmap/zcrypto/x509/pkix"
+
+	ct "github.com/google/certificate-transparency-go"
+	cttls "github.com/google/certificate-transparency-go/tls"
+	ctx509 "github.com/google/certificate-transparency-go/x509"
+)
+
+// 参考：https://github.com/cloudflare/cfssl/blob/master/certdb/ocspstapling/ocspstapling.go
+
+// sctExtOid is the OID of the OCSP Stapling SCT extension (see section 3.3. of RFC 6962).
+var sctExtOid = asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 11129, 2, 4, 5}
+
+type NewParsedAndRawSCT struct {
+	Raw []byte
+	Parsed *ct.SignedCertificateTimestamp
+}
+
+// SerializeSCTList serializes a list of SCTs.
+func SerializeSCTList(sctList []ct.SignedCertificateTimestamp) ([]byte, error) {
+	list := ctx509.SignedCertificateTimestampList{}
+	for _, sct := range sctList {
+		sctBytes, err := cttls.Marshal(sct)
+		if err != nil {
+			return nil, err
+		}
+		list.SCTList = append(list.SCTList, ctx509.SerializedSCT{Val: sctBytes})
+	}
+	return cttls.Marshal(list)
+}
+
+// DeserializeSCTList deserializes a list of SCTs.
+func DeserializeSCTList(serializedSCTList []byte) (int,int,[]NewParsedAndRawSCT, error) {
+	var sctLen int = 0
+	var sctNum int = 0
+
+	var sctList ctx509.SignedCertificateTimestampList
+	rest, err := cttls.Unmarshal(serializedSCTList, &sctList)
+	if err != nil {
+		return sctLen,sctNum,nil, err
+	}
+	if len(rest) != 0 {
+		return sctLen,sctNum,nil,errors.New("serialized SCT list contained trailing garbage")
+	}
+	list := make([]NewParsedAndRawSCT, len(sctList.SCTList))
+	for i, serializedSCT := range sctList.SCTList {
+		sctLen += len(serializedSCT.Val)
+		var npars NewParsedAndRawSCT
+		var sct ct.SignedCertificateTimestamp
+		rest, err := cttls.Unmarshal(serializedSCT.Val, &sct)
+		if err != nil {
+			return sctLen,sctNum,nil, err
+		}
+		if len(rest) != 0 {
+			return sctLen,sctNum,nil, errors.New("serialized SCT contained trailing garbage")
+		}
+		npars.Raw = serializedSCT.Val
+		npars.Parsed = &sct
+		list[i] = npars
+	}
+	sctNum = len(list)
+	return sctLen,sctNum,list, nil
+}
+
+// 将string格式转为 Response结构体
+func ConvertResponse(stringResponse string)(*Response,error) {
+	der, err_base64_encoded:= base64.StdEncoding.DecodeString(stringResponse)
+	if err_base64_encoded != nil{
+		return nil, err_base64_encoded
+	}
+	// 解析response
+	response, err_parse_der := ParseResponseForCert(der,nil,nil)
+	if err_parse_der != nil {
+		return nil,err_parse_der
+	}
+
+	return response,nil
+
+}
+
+// 解析传递过来的ocsp response,解析得到sct by ocsp stapling
+func ParseSCTListFromOcspResponse(response *Response)(int,int,[]NewParsedAndRawSCT,error){
+	// 将response中的sct扩展提取出来
+	var sctExt pkix.Extension
+	for _,ext := range response.Extensions {
+		if ext.Id.Equal(sctExtOid){
+			sctExt = ext
+			break
+		}
+	}
+
+	// 提取sct
+	sctlist_byte := sctExt.Value
+
+	// 反序列化为signed certificate timestamp
+	var sctList []NewParsedAndRawSCT
+	var sctLen int = 0
+	var sctNum int = 0
+	var err_sct_seri error
+	var err_end error
+	if numBytes := len(sctlist_byte); numBytes != 0 {
+		var serializedSCTList []byte
+		rest := make([]byte, numBytes)
+		copy(rest,sctlist_byte)
+		for len(rest) != 0 {
+			rest, err_sct_seri = asn1.Unmarshal(rest, &serializedSCTList)
+			if err_sct_seri != nil {
+				return sctLen,sctNum,nil,err_sct_seri
+			}
+		}
+		sctLen,sctNum,sctList,err_end = DeserializeSCTList(serializedSCTList)
+	}
+
+	return sctLen,sctNum,sctList, err_end
+}
